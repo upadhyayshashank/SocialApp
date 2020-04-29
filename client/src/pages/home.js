bb9435fc129b3b66916b12e2bdb42ea6d94be077
Shashank-srh <11013757@stud.hochschule-heidelberg.de>
2020-04-29 16:46:52 +0530

    Like, Unlke and delete post actions

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -3,35 +3,30 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
+import PropTypes from 'prop-types';
 
 import Scream from "../components/Scream";
 import Profile from "../components/Profile";
 
+import { connect } from 'react-redux';
+import { getScreams } from '../redux/actions/dataActions';
+
 class home extends Component {
-	state = {
-		screams: null,
-	};
 	componentDidMount() {
-		axios
-			.get("/screams")
-			.then((res) => {
-				this.setState({
-					screams: res.data,
-				});
-			})
-			.catch((err) => {
-				console.log(err);
-			});
+		this.props.getScreams();
 	}
 
 	render() {
-		let recentScreamsMarkup = this.state.screams ? (
-			this.state.screams.map((screams) => (
+		const { screams, loading } = this.props.data;
+		let recentScreamsMarkup = !loading ? (
+			screams.map((screams) => 
 				<Scream key={screams.screamId} screams={screams} />
-			))
+			)
 		) : (
-			<p>Loading..</p>
-		);
+		<p>Loading..</p>
+			);
+			
+		
 		return (
 			<Grid container spacing={4}>
 				<Grid item sm={8} xs={12}>
@@ -45,4 +40,16 @@ class home extends Component {
 	}
 }
 
-export default home;
+home.propTypes = {
+	getScreams: PropTypes.func.isRequired,
+	data: PropTypes.object.isRequired
+  };
+  
+  const mapStateToProps = (state) => ({
+	data: state.data
+  });
+  
+  export default connect(
+	mapStateToProps,
+	{ getScreams }
+  )(home);
